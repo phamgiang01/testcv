6cc29cf63ece45f27e90c0e65660e5433805d60b
phamgiang01 <pgisme07@gmail.com>
2022-05-26 22:52:41 +0700

    test2

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
+let list = ["one", "two", "three", "four", "five", "six"];
+let len = "length";
+let lengthObj = list.reduce((acc, curr) => {
+  const lengthKey = curr.length;
+  if (acc[lengthKey]) {
+    acc[lengthKey].push(curr);
+  } else {
+    acc[lengthKey] = [curr];
+  }
 
-
-let arr = ["one","two","three","four","five","six"];
-var arrIndex = []
-
-let maxLength = 1;
-for(let i =0;i<arr.length;i++){
-  if(arr[i].length>=maxLength) maxLength = arr[i].length
-}
-
-// for(let i=0;i<arr.length;i++){
-//   if(arr)
-// }
-
-console.log(maxLength)
-
-
-// for (var i = 1; i < arr.length; i++) {
-//   console.log([i])
-//   // if(arr)
-//   // if (arr) team1.push(arr[i]);
-//   // if ([i] > Math.floor(arr.length / 3) && [i] <= arr.length - 3)
-//   //   team2.push(arr[i]);
-//   // if ([i] > arr.length - 3) team3.push(arr[i]);
-// }
-
+  return acc;
+}, {});
